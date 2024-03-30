b80b3039ef2b43ae29a5b09e4121fe0ac1007e24
jusnguyen <jeytech@protonmail.com>
2024-03-30 16:48:31 -0700

    game and music player holders

---
+++ components/Game.tsx
@@ -0,0 +1,11 @@
+const Game: React.FC = () => {
+    return (
+      <div>
+        {/* Placeholder content for Game component */}
+        <h2>Game Component</h2>
+      </div>
+    );
+  };
+  
+  export default Game;
+  
---
+++ components/MusicPlayer.tsx
@@ -0,0 +1,12 @@
+const MusicPlayer: React.FC = () => {
+    return (
+      <div>
+        {/* Placeholder content for MusicPlayer component */}
+        <h2>Music Player</h2>
+        {/* You can add more basic structure or styling here */}
+      </div>
+    );
+  };
+  
+  export default MusicPlayer;
+  
