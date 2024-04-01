008f46a0a5272d207544d89f97f1aa281041b2b5
jusnguyen <jeytech@protonmail.com>
2024-03-31 17:28:56 -0700

    css fix and vision

--- pages/vision.tsx
+++ pages/vision.tsx
@@ -11,14 +11,15 @@ const VisionPage: React.FC = () => {
 
       <Navigation />
 
-      {/* Wallpaper Section */}
       <div className="relative w-full min-h-screen">
         <Image
           src="/images/wallpapers/coastal_banner.png"
           alt="Coastal Banner Wallpaper"
           layout="fill"
           objectFit="cover"
+          priority // Helps load the image faster
         />
+
         <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
           <main className="p-8 text-center max-w-5xl mx-auto space-y-12">
             <h1 className="text-6xl font-bold mb-8">Embark on a Netsoul Odyssey</h1>
@@ -26,7 +27,6 @@ const VisionPage: React.FC = () => {
               Netsoul is not just a game; it&apos;s a portal to a universe where adventure and learning merge, guiding you through a surreal journey across the vast expanse of cyberspace.
             </p>
 
-            {/* Free to Play, Anywhere and Everywhere */}
             <section className="mb-10">
               <h2 className="text-4xl font-bold mb-4">Free to Play, Anywhere and Everywhere</h2>
               <p className="text-lg">
@@ -34,7 +34,6 @@ const VisionPage: React.FC = () => {
               </p>
             </section>
 
-            {/* Cross-Chain Gaming Mechanics */}
             <section className="mb-10">
               <h2 className="text-4xl font-bold mb-4">Cross-Chain Gaming Mechanics</h2>
               <p className="text-lg">
@@ -42,7 +41,6 @@ const VisionPage: React.FC = () => {
               </p>
             </section>
 
-            {/* Retro Art Style */}
             <section className="mb-10">
               <h2 className="text-4xl font-bold mb-4">Retro Art Style</h2>
               <p className="text-lg">
@@ -50,7 +48,6 @@ const VisionPage: React.FC = () => {
               </p>
             </section>
 
-            {/* Gamified Learning */}
             <section className="mb-10">
               <h2 className="text-4xl font-bold mb-4">Gamified Learning</h2>
               <p className="text-lg">
@@ -58,16 +55,14 @@ const VisionPage: React.FC = () => {
               </p>
             </section>
 
-
-            {/* Lab Research Graphic */}
             <div className="my-10">
               <Image
                 src="/images/wallpapers/lab_research.png"
                 alt="Lab Research"
-                layout='responsive'  // Adjusting for a responsive layout
-                width={800}  // Provide specific width
-                height={450}  // Provide specific height
-                className="mx-auto rounded-lg shadow-lg"  // Additional styling for a polished look
+                layout='responsive'
+                width={800}
+                height={450}
+                className="mx-auto rounded-lg shadow-lg"
               />
             </div>
 
@@ -84,7 +79,6 @@ const VisionPage: React.FC = () => {
                 Be part of a community-driven universe where players and developers collectively shape the Netsoul world, making every encounter unique and dynamic.
               </p>
             </section>
-
           </main>
         </div>
       </div>
