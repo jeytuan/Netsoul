825a8af537c49a35a3b7f49466ab2f0ccab14dfd
jusnguyen <jeytech@protonmail.com>
2024-04-06 18:23:39 -0700

    Update DemoScene.tsx

--- components/DemoScene.tsx
+++ components/DemoScene.tsx
@@ -5,8 +5,8 @@ const DemoScene: React.FC = () => {
   useEffect(() => {
     const config: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
-      width: 1300, // Adjust as needed
-      height: 750, // Adjust as needed
+      width: 1300,
+      height: 750,
       parent: 'phaser-game-container',
       physics: {
         default: 'arcade',
@@ -22,19 +22,18 @@ const DemoScene: React.FC = () => {
           this.load.image('spriteTRON', '/images/game/bosses/TRON.png');
         },
         create: function (this: Phaser.Scene) {
-          // Add grid to the scene
-          const grid = this.add.image(0, 0, 'grid').setOrigin(0, 0);
-          // Scale the grid to fit the game dimensions
-          grid.displayWidth = this.sys.canvas.width;
-          grid.displayHeight = this.sys.canvas.height;
+          const cols = 6;
+          const rows = 3;
+          const cellWidth = this.scale.width / cols;
+          const cellHeight = this.scale.height / rows;
 
-          // Add SKALE sprite to the scene
-          const spriteSKALE = this.add.sprite(200, 300, 'spriteSKALE');
-          spriteSKALE.setInteractive(); // Makes the sprite interactive (optional)
+          const grid = this.add.image(0, 0, 'grid').setOrigin(0, 0).setDisplaySize(this.scale.width, this.scale.height);
 
-          // Add TRON sprite to the scene
-          const spriteTRON = this.add.sprite(600, 300, 'spriteTRON');
-          spriteTRON.setInteractive(); // Makes the sprite interactive (optional)
+          const spriteSKALE = this.add.sprite(cellWidth * 4.5, cellHeight * 1.5, 'spriteSKALE').setScale(0.8);
+          spriteSKALE.setInteractive();
+
+          const spriteTRON = this.add.sprite(cellWidth * 1.5, cellHeight * 1.5, 'spriteTRON').setScale(0.25);
+          spriteTRON.setInteractive();
         }
       }
     };
