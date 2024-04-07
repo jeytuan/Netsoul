b6fa63e6f8f81f53aa310346ac5ca1eb10b27e3f
jusnguyen <jeytech@protonmail.com>
2024-04-06 17:56:24 -0700

    Update DemoScene.tsx

--- components/DemoScene.tsx
+++ components/DemoScene.tsx
@@ -3,11 +3,11 @@ import Phaser from 'phaser';
 
 const DemoScene: React.FC = () => {
   useEffect(() => {
-    const config = {
+    const config: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
-      width: 800, // Adjust to match grid size
-      height: 400, // Adjust to match grid size
-      parent: 'phaser-game', // Div ID where Phaser injects the canvas
+      width: 800,
+      height: 600,
+      parent: 'phaser-game-container', // Make sure Phaser canvas is injected into the correct container
       physics: {
         default: 'arcade',
         arcade: {
@@ -16,30 +16,21 @@ const DemoScene: React.FC = () => {
         }
       },
       scene: {
-        preload: preload,
-        create: create
+        preload: function (this: Phaser.Scene) {
+          this.load.image('grid', '/images/game/platforms/grid.png');
+          this.load.image('spriteSKALE', '/images/game/bosses/SKALE.png');
+          this.load.image('spriteTRON', '/images/game/bosses/TRON.png');
+        },
+        create: function (this: Phaser.Scene) {
+          // Create the game scene
+        }
       }
     };
 
     new Phaser.Game(config);
-
-    function preload() {
-      this.load.image('grid', '/images/game/platforms/grid.png');
-      this.load.image('spriteSKALE', '/images/game/bosses/SKALE.png');
-      this.load.image('spriteTRON', '/images/game/bosses/TRON.png');
-    }
-
-    function create() {
-      // Background grid
-      this.add.image(400, 200, 'grid').setScale(0.5); // Adjust scale as needed
-
-      // Sprites
-      this.add.image(200, 200, 'spriteSKALE').setScale(0.5); // Position and scale sprite
-      this.add.image(600, 200, 'spriteTRON').setScale(0.5); // Position and scale sprite
-    }
   }, []);
 
-  return <div id="phaser-game" style={{ width: '800px', height: '400px', margin: '0 auto' }} />;
+  return <div id="phaser-game-container" />;
 };
 
 export default DemoScene;
