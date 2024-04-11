514afcb6a53d8e6683d91b224853705ae2cd7506
jusnguyen <jeytech@protonmail.com>
2024-04-10 18:28:55 -0700

    Update DemoScene.tsx

--- components/DemoScene.tsx
+++ components/DemoScene.tsx
@@ -22,38 +22,32 @@ const DemoScene: React.FC = () => {
           this.load.image('grid', '/images/game/platforms/grid.png');
           this.load.image('spriteSKALE', '/images/game/bosses/SKALE.png');
           this.load.image('spriteTRON', '/images/game/bosses/TRON.png');
-          this.load.image('spriteEtherlink', '/images/game/bosses/Etherlink1.png');
         },
         create: function (this: Phaser.Scene) {
           const cols = 6;
           const rows = 3;
           const cellWidth = this.scale.width / cols;
           const cellHeight = this.scale.height / rows;
+          const yOffsetSKALE = -100; // Y-offset for SKALE
+          const yOffsetTRON = -75; // Y-offset for TRON, reduced by 25px
 
           this.add.image(0, 0, 'grid').setOrigin(0, 0).setDisplaySize(this.scale.width, this.scale.height);
 
-          const spriteSKALE = this.add.sprite(cellWidth * 4.5, cellHeight * 1.5, 'spriteSKALE').setScale(0.6).setInteractive();
-          const spriteEtherlink = this.add.sprite(cellWidth * 4.5, cellHeight * 1.5, 'spriteEtherlink').setScale(0.6).setInteractive().setVisible(false);
+          const spriteSKALE = this.add.sprite(cellWidth * 4.5, (cellHeight * 1.5) + yOffsetSKALE, 'spriteSKALE').setScale(0.6).setInteractive().setVisible(currentBoss === 'spriteSKALE');
+          const spriteTRON = this.add.sprite(cellWidth * 1.5, (cellHeight * 1.5) + yOffsetTRON, 'spriteTRON').setScale(0.25).setInteractive().setVisible(currentBoss === 'spriteTRON');
 
-          this.data.set('currentBoss', 'spriteSKALE');
-
-          // Define the toggleBoss function inside the create method
-          const toggleBoss = (bossName: string) => {
-            this.data.set('currentBoss', bossName);
-            spriteSKALE.setVisible(bossName === 'spriteSKALE');
-            spriteEtherlink.setVisible(bossName === 'spriteEtherlink');
-          };
-
-          // Initialize with the default boss
-          toggleBoss('spriteSKALE');
+          this.data.set('currentBoss', currentBoss);
         },
         update: function (this: Phaser.Scene) {
-          const currentBossInScene = this.data.get('currentBoss');
-          if (currentBossInScene !== currentBoss) {
-            // The toggleBoss function needs to be accessible here, so you might need to adjust this logic.
-            // Consider using an approach that doesn't require direct access to toggleBoss here.
+          if (this.data.get('currentBoss') !== currentBoss) {
+            this.data.set('currentBoss', currentBoss);
+            this.children.each((gameObject) => {
+              if (gameObject instanceof Phaser.GameObjects.Sprite) {
+                gameObject.setVisible(gameObject.texture.key === `sprite${currentBoss}`);
+              }
+            });
           }
-        }
+        },
       },
     });
 
@@ -66,7 +60,7 @@ const DemoScene: React.FC = () => {
     <div>
       <div style={{ position: 'absolute', top: '10px', left: '10px' }}>
         <button onClick={() => setCurrentBoss('spriteSKALE')}>SKALE Boss</button>
-        <button onClick={() => setCurrentBoss('spriteEtherlink')}>Etherlink Boss</button>
+        <button onClick={() => setCurrentBoss('spriteTRON')}>TRON Boss</button>
       </div>
       <div id="phaser-game-container" />
     </div>
