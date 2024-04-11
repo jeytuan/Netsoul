e9e2f619de2f7df727e5b5312caf972a51ca4ca6
jusnguyen <jeytech@protonmail.com>
2024-04-10 18:49:00 -0700

    Update DemoScene.tsx

--- components/DemoScene.tsx
+++ components/DemoScene.tsx
@@ -31,12 +31,12 @@ const DemoScene: React.FC = () => {
           const cellHeight = this.scale.height / rows;
           
           // Adjust Y-offsets for SKALE and TRON
-          const yOffsetSKALE = -100 + 20; // Increased by 20px for SKALE
-          const yOffsetTRON = -75 - 30; // Decreased by 30px for TRON
+          const yOffsetSKALE = -100 + 20;
+          const yOffsetTRON = -75 - 30 + 20; // Decreased by 30px and increased by 20px for TRON
 
           this.add.image(0, 0, 'grid').setOrigin(0, 0).setDisplaySize(this.scale.width, this.scale.height);
 
-          const spriteTRON = this.add.sprite(cellWidth * 1.5, (cellHeight * 1.5) + yOffsetTRON, 'spriteTRON').setScale(0.25).setInteractive();
+          const spriteTRON = this.add.sprite(cellWidth * 1.5, (cellHeight * 1.5) + yOffsetTRON, 'spriteTRON').setScale(0.25 * 1.2).setInteractive(); // Scale increased by 20%
           const spriteSKALE = this.add.sprite(cellWidth * 4.5, (cellHeight * 1.5) + yOffsetSKALE, 'spriteSKALE').setScale(0.6).setInteractive().setVisible(currentBoss === 'spriteSKALE');
           const spriteEtherlink = this.add.sprite(cellWidth * 4.5, (cellHeight * 1.5) + yOffsetSKALE, 'spriteEtherlink').setScale(0.6).setInteractive().setVisible(currentBoss === 'spriteEtherlink');
 
